b517af111dbd488d236d72ed559607e984d5dc58
Artem <artem.egorov.art2006@gmail.com>
2024-02-12 23:48:00 +0300

    Добавил логику создание вопросов и Справка

--- Forms/MainForm.Designer.cs
+++ Forms/MainForm.Designer.cs
@@ -30,9 +30,14 @@
         {
             this.MainMenuStrip = new System.Windows.Forms.MenuStrip();
             this.FileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.ReferenceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.EditingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.StartEditingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.PassingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.StartPassingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ReferenceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createQuestionPanel2 = new FreeTest.Controls.CreateQuestionPanel();
             this.MainMenuStrip.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -47,15 +52,60 @@
             this.ReferenceToolStripMenuItem});
             this.MainMenuStrip.Location = new System.Drawing.Point(0, 0);
             this.MainMenuStrip.Name = "MainMenuStrip";
-            this.MainMenuStrip.Size = new System.Drawing.Size(689, 28);
+            this.MainMenuStrip.Size = new System.Drawing.Size(765, 28);
             this.MainMenuStrip.TabIndex = 0;
             this.MainMenuStrip.Text = "menuStrip1";
             // 
             // FileToolStripMenuItem
             // 
+            this.FileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
             this.FileToolStripMenuItem.Name = "FileToolStripMenuItem";
             this.FileToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
             this.FileToolStripMenuItem.Text = "Файл";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(166, 26);
+            this.openToolStripMenuItem.Text = "Открыть";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(166, 26);
+            this.saveToolStripMenuItem.Text = "Сохранить";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // EditingToolStripMenuItem
+            // 
+            this.EditingToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StartEditingToolStripMenuItem});
+            this.EditingToolStripMenuItem.Name = "EditingToolStripMenuItem";
+            this.EditingToolStripMenuItem.Size = new System.Drawing.Size(137, 24);
+            this.EditingToolStripMenuItem.Text = "Редактирование";
+            // 
+            // StartEditingToolStripMenuItem
+            // 
+            this.StartEditingToolStripMenuItem.Name = "StartEditingToolStripMenuItem";
+            this.StartEditingToolStripMenuItem.Size = new System.Drawing.Size(260, 26);
+            this.StartEditingToolStripMenuItem.Text = "Начать редактирование";
+            // 
+            // PassingToolStripMenuItem
+            // 
+            this.PassingToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StartPassingToolStripMenuItem});
+            this.PassingToolStripMenuItem.Name = "PassingToolStripMenuItem";
+            this.PassingToolStripMenuItem.Size = new System.Drawing.Size(121, 24);
+            this.PassingToolStripMenuItem.Text = "Прохождение";
+            // 
+            // StartPassingToolStripMenuItem
+            // 
+            this.StartPassingToolStripMenuItem.Name = "StartPassingToolStripMenuItem";
+            this.StartPassingToolStripMenuItem.Size = new System.Drawing.Size(241, 26);
+            this.StartPassingToolStripMenuItem.Text = "Начать прохождение";
             // 
             // ReferenceToolStripMenuItem
             // 
@@ -64,26 +114,22 @@
             this.ReferenceToolStripMenuItem.Text = "Справка";
             this.ReferenceToolStripMenuItem.Click += new System.EventHandler(this.ReferenceToolStripMenuItem_Click);
             // 
-            // EditingToolStripMenuItem
+            // createQuestionPanel2
             // 
-            this.EditingToolStripMenuItem.Name = "EditingToolStripMenuItem";
-            this.EditingToolStripMenuItem.Size = new System.Drawing.Size(137, 24);
-            this.EditingToolStripMenuItem.Text = "Редактирование";
-            // 
-            // PassingToolStripMenuItem
-            // 
-            this.PassingToolStripMenuItem.Name = "PassingToolStripMenuItem";
-            this.PassingToolStripMenuItem.Size = new System.Drawing.Size(121, 24);
-            this.PassingToolStripMenuItem.Text = "Прохождение";
+            this.createQuestionPanel2.Location = new System.Drawing.Point(2, 31);
+            this.createQuestionPanel2.Name = "createQuestionPanel2";
+            this.createQuestionPanel2.Size = new System.Drawing.Size(763, 531);
+            this.createQuestionPanel2.TabIndex = 4;
+            this.createQuestionPanel2.QuesionCreated += new FreeTest.Controls.CreateQuestionPanel.QuestionCreatedHandler(this.createQuestionPanel2_QuesionCreated);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.ClientSize = new System.Drawing.Size(689, 415);
+            this.ClientSize = new System.Drawing.Size(765, 561);
+            this.Controls.Add(this.createQuestionPanel2);
             this.Controls.Add(this.MainMenuStrip);
-            this.MainMenuStrip = this.MainMenuStrip;
             this.Name = "MainForm";
             this.Text = "FreeTest";
             this.MainMenuStrip.ResumeLayout(false);
@@ -100,6 +146,11 @@
         private System.Windows.Forms.ToolStripMenuItem ReferenceToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem EditingToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem PassingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem StartEditingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem StartPassingToolStripMenuItem;
+        private Controls.CreateQuestionPanel createQuestionPanel2;
     }
 }
 
--- Forms/MainForm.cs
+++ Forms/MainForm.cs
@@ -1,27 +1,96 @@
 ﻿using FreeTest.Forms;
+using FreeTestManager.Core.Builders.TestBuilder.Implementations;
+using FreeTestManager.Core.ManagerInterfaces;
+using FreeTestManager.Core.Providers.Implementations;
+using FreeTestManager.Entities;
+using FreeTestManager.TestBuilder;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FreeTest
 {
     public partial class MainForm : Form
     {
+        public Test Test { get; set; }
+        private readonly ITestProvider testProvider;
+        private readonly ITestBuilder testBuilder;
         public MainForm()
         {
             InitializeComponent();
+            Test = new Test();
+            testBuilder = new FreeTestBuilder();
+            testProvider = new FileTestProvider();
         }
 
         private void ReferenceToolStripMenuItem_Click(object sender, EventArgs e)
         {
             AboutForm aboutForm = new AboutForm();
             aboutForm.ShowDialog();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string fileName = showFileDialogForOpenTask();
+
+            if (fileName != null) 
+            {
+                MessageBox.Show(
+                    "Не удалось открыть файл", 
+                    "Ошибка",
+                    MessageBoxButtons.OK
+                );
+            }
+
+            Test = testProvider.Load(fileName.Split('.')[0]);
+        }
+
+        private string showFileDialogForOpenTask()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = "Json File|*.json";
+            openFileDialog.InitialDirectory = testProvider.BaseDirectoryPath;
+
+            openFileDialog.ShowDialog();
+
+            return openFileDialog.FileName;
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (Test is null && string.IsNullOrWhiteSpace(Test.Title))
+            {
+                MessageBox.Show(
+                    "Не удалось сохранить тест. Пустое название теста.",
+                    "Ошибка",
+                    MessageBoxButtons.OK
+                );
+            }
+
+            bool result = testProvider.Save(Test);
+
+            if (result)
+            {
+                MessageBox.Show(
+                    "Тест сохранён успешно.",
+                    "Успешно",
+                    MessageBoxButtons.OK
+                );
+            }
+            else
+            {
+                MessageBox.Show(
+                    "Тест не сохранён успешно.",
+                    "Ошибка",
+                    MessageBoxButtons.OK
+                );
+            }
+        }
+
+        private void createQuestionPanel2_QuesionCreated(Question question)
+        {
+            testBuilder.AddQuestion(question);
+            MessageBox.Show(testBuilder.GetResult().Questions.Count.ToString());
         }
     }
 }
